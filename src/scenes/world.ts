9ae7a6a7c94bb7a844e0d677b7354e3220923250
Victor Debone <victor@debone.com.br>
2023-04-19 02:28:23 +0200

    Moved to tilemaps

--- src/scenes/world.ts
+++ src/scenes/world.ts
@@ -4,7 +4,7 @@ import PhaserGamebus from "../gamebus";
 
 import Phaser, { Display } from "phaser";
 import { Map } from "../systems/map";
-import { RESOURCES } from "./main";
+//import { RESOURCES } from "./main";
 import { Player } from "../entities/player";
 
 type Color = Display.Color;
@@ -13,10 +13,10 @@ const Color = Display.Color;
 export const tileSizeWidth = 64;
 export const tileSizeHeight = 32;
 
-export const worldSize = 256;
+export const worldSize = 512;
 
 export const playerStartX = 95;
-export const playerStartY = 135;
+export const playerStartY = 35;
 
 export class SceneWorld extends Phaser.Scene {
   declare controls: Phaser.Cameras.Controls.SmoothedKeyControl;
@@ -39,7 +39,7 @@ export class SceneWorld extends Phaser.Scene {
     graphics: Phaser.GameObjects.Graphics
   ) {
     // this.floorLayer?
-    const worldCoord = this.map.tileToWorld(x, y);
+    const worldCoord = this.map.map.tileToWorldXY(x, y)!;
     //const tileAt = this.map.getTile(x, y);
 
     // Snap to tile coordinates, but in world space
@@ -47,13 +47,13 @@ export class SceneWorld extends Phaser.Scene {
     let ty = worldCoord.y;
 
     graphics.lineStyle(7, color.color, 1);
-    graphics.translateCanvas(tx, ty);
+    graphics.translateCanvas(tx, ty + tileSizeHeight);
     graphics.beginPath();
-    graphics.moveTo(0, 0);
-    graphics.lineTo(tileSizeWidth / 2, tileSizeHeight / 2);
-    graphics.lineTo(0, tileSizeHeight);
-    graphics.lineTo(-tileSizeWidth / 2, tileSizeHeight / 2);
-    graphics.lineTo(0, 0);
+    graphics.moveTo(tileSizeWidth / 2, 0);
+    graphics.lineTo(tileSizeWidth, tileSizeHeight / 2);
+    graphics.lineTo(tileSizeWidth / 2, tileSizeHeight);
+    graphics.lineTo(0, tileSizeHeight / 2);
+    graphics.lineTo(tileSizeWidth / 2, 0);
     graphics.closePath();
     graphics.strokePath();
   }
@@ -89,7 +89,7 @@ export class SceneWorld extends Phaser.Scene {
 
     this.cursors = this.input.keyboard!.createCursorKeys();
 
-    //    this.cameras.main.setZoom(0.7);
+    //this.cameras.main.setZoom(0.1);
 
     /*const controlConfig = {
       camera: this.cameras.main,
@@ -117,16 +117,10 @@ export class SceneWorld extends Phaser.Scene {
   declare playerStart: Phaser.Math.Vector2;
 
   createGame() {
-    this.map = new Map(this.cameras.main, tileSizeWidth, tileSizeHeight);
+    this.map = new Map(this, tileSizeWidth, tileSizeHeight);
 
-    this.playerStart = this.map.tileToWorld(playerStartX, playerStartY);
+    this.playerStart = this.map.map.tileToWorldXY(playerStartX, playerStartY)!;
     //this.cameras.main.centerOn(this.playerStart.x, this.playerStart.y);
-
-    for (let x = 0; x < worldSize; x++) {
-      for (let y = 0; y < worldSize; y++) {
-        this.drawIsoTile(x, y, 0xffff00 - 10000 * this.map.getTile(x, y));
-      }
-    }
 
     this.player = new Player(
       this,
@@ -142,14 +136,6 @@ export class SceneWorld extends Phaser.Scene {
     this.marker = this.add.graphics();
     //this.player.body?.drawDebug(this.marker);
     this.drawMark(7, 7, Color.RandomRGB(), this.marker);
-  }
-
-  // Function that draws an isometric tile at the given position
-  drawIsoTile(tileX: number, tileY: number, color: number) {
-    const { x, y } = this.map.tileToWorld(tileX, tileY);
-
-    this.add.sprite(x, y + tileSizeHeight / 2, RESOURCES.TEST_TILE).tint =
-      color;
   }
 
   update(_time: number) {
@@ -179,9 +165,10 @@ export class SceneWorld extends Phaser.Scene {
       this.cameras.main
     ) as Phaser.Math.Vector2;
 
-    const pointerTile = this.map.worldToTile(
+    const pointerTile = this.map.map.worldToTileXY(
       worldPoint.x,
-      worldPoint.y //- this.map.tileWidth / 2,
+      worldPoint.y, //- this.map.tileWidth / 2,,
+      true
     );
 
     //    console.log(this.map.getTileAt(pointerTile.x, pointerTile.y));
@@ -206,7 +193,10 @@ export class SceneWorld extends Phaser.Scene {
       this.params.tileCoord.y = pointerTile.y;
       this.params.worldCoord.x = worldPoint.x;
       this.params.worldCoord.y = worldPoint.y;
-      this.params.tile = this.map.getTile(pointerTile.x, pointerTile.y);
+      this.params.tile = this.map.map.getTileAt(
+        Math.floor(pointerTile.x),
+        Math.floor(pointerTile.y)
+      );
 
       this.params.water = this.player.water;
       this.params.stamina = this.player.stamina;
--- src/systems/map.ts
+++ src/systems/map.ts
@@ -1,44 +1,62 @@
 import Phaser from "phaser";
-import { tileSizeHeight, tileSizeWidth, worldSize } from "../scenes/world";
+import {
+  SceneWorld,
+  tileSizeHeight,
+  tileSizeWidth,
+  worldSize,
+} from "../scenes/world";
 
-const Vector2 = Phaser.Math.Vector2;
-type Vector2 = Phaser.Math.Vector2;
-type Camera = Phaser.Cameras.Scene2D.Camera;
-
-import Perlin from "phaser3-rex-plugins/plugins/perlin.js";
+//import Perlin from "phaser3-rex-plugins/plugins/perlin.js";
+import { RESOURCES } from "../scenes/main";
 
 export class Map {
-  camera: Camera;
-
   tileWidth = tileSizeWidth;
   tileHeight = tileSizeHeight;
 
-  map: number[][] = [];
+  map: Phaser.Tilemaps.Tilemap;
+  mapData: Phaser.Tilemaps.MapData;
 
-  constructor(camera: Camera, tileWidth: number, tileHeight: number) {
-    this.camera = camera;
+  constructor(scene: SceneWorld, tileWidth: number, tileHeight: number) {
     this.tileWidth = tileWidth;
     this.tileHeight = tileHeight;
 
-    this.generateMap(worldSize, 50);
+    this.mapData = new Phaser.Tilemaps.MapData({
+      width: worldSize,
+      height: worldSize,
+      tileWidth: tileSizeWidth,
+      tileHeight: tileSizeHeight,
+      orientation: Phaser.Tilemaps.Orientation.ISOMETRIC,
+      format: Phaser.Tilemaps.Formats.ARRAY_2D,
+    });
 
-    this.defineRoom(93, 132, 98, 137, -1);
+    this.map = new Phaser.Tilemaps.Tilemap(scene, this.mapData);
 
-    this.defineRoom(91, 123, 92, 124, -15);
-    this.defineRoom(86, 137, 87, 138, -15);
+    const tileset = this.map.addTilesetImage(
+      "iso-64x64-outside",
+      RESOURCES.TEST_TILE
+    );
+
+    const layer = this.map.createBlankLayer("layer", tileset!, 0, 0);
+
+    layer?.fill(0, 0, 0, worldSize, worldSize);
+
+    //this.generateMap(worldSize, 50);
+
+    //this.defineRoom(93, 132, 98, 137, -1);
+
+    //this.defineRoom(91, 123, 92, 124, -15);
+    //this.defineRoom(86, 137, 87, 138, -15);
   }
 
-  generateMap(width: number, gradient: number) {
-    const noise = new Perlin(1);
-    const noise2 = new Perlin(100);
-    this.map = [];
+  generateMap(width: number /*gradient: number*/) {
+    //const noise = new Perlin(1);
+    //const noise2 = new Perlin(100);
     for (let y = 0; y < width; y++) {
-      this.map.push([]);
       for (let x = 0; x < width; x++) {
-        this.map[y].push(
+        /*this.map[y].push(
           noise2.perlin2(x / (gradient * 10), y / (gradient * 10)) * 20 +
             noise.simplex2(x / gradient, y / gradient)
-        );
+        );*/
       }
     }
   }
@@ -52,59 +70,13 @@ export class Map {
     topLeftX: number,
     topLeftY: number,
     bottomRightX: number,
-    bottomRightY: number,
-    value: number
+    bottomRightY: number
+    //value: number
   ) {
     for (let x = topLeftX; x < bottomRightX; x++) {
       for (let y = topLeftY; y < bottomRightY; y++) {
-        this.map[y][x] = value;
+        //  this.map[y][x] = value;
       }
     }
-  }
-
-  getTile(x: number, y: number) {
-    return this.map?.[y]?.[x];
-  }
-
-  worldToTile(worldX: number, worldY: number, point: Vector2 = new Vector2()) {
-    // Find the world position relative to the static or dynamic layer's top left origin,
-    // factoring in the camera's vertical scroll
-    /*worldY =
-      worldY -
-      (tilemapLayer.y + camera.scrollY * (1 - tilemapLayer.scrollFactorY));
-
-    tileHeight *= tilemapLayer.scaleY;
-
-    // Find the world position relative to the static or dynamic layer's top left origin,
-    // factoring in the camera's horizontal scroll
-
-    worldX =
-      worldX -
-      (tilemapLayer.x + camera.scrollX * (1 - tilemapLayer.scrollFactorX));
-
-    tileWidth *= tilemapLayer.scaleX;*/
-
-    const tileWidthHalf = this.tileWidth / 2;
-    const tileHeightHalf = this.tileHeight / 2;
-
-    //    worldX = worldX - tileWidthHalf;
-
-    var x = 0.5 * (worldX / tileWidthHalf + worldY / tileHeightHalf);
-    var y = 0.5 * (-worldX / tileWidthHalf + worldY / tileHeightHalf);
-
-    x = Math.floor(x);
-    y = Math.floor(y);
-
-    return point.set(x, y);
-  }
-
-  tileToWorld(tileX: number, tileY: number, point: Vector2 = new Vector2()) {
-    var layerWorldX = 0;
-    var layerWorldY = 0;
-
-    var x = layerWorldX + (tileX - tileY) * (this.tileWidth / 2);
-    var y = layerWorldY + (tileX + tileY) * (this.tileHeight / 2);
-
-    return point.set(x, y);
   }
 }
